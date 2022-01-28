61607531fa296ee5e67cdbbcdca67c571d77476c
Frank Kulak <fs.kulak@gmail.com>
2022-01-27 22:47:23 -0500

    Add onboarding overlay.

---
+++ src/components/shared/BlurOverlay.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+
+  export let fill: boolean = true;
+
+  const bodyClassName = "blur-effect-active";
+
+  onMount(() => {
+    document.body.classList.add(bodyClassName);
+  });
+
+  onDestroy(() => {
+    document.body.classList.remove(bodyClassName);
+  });
+</script>
+
+<div class="overlay">
+  <div class="popup" class:fill>
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  .overlay {
+    z-index: 50000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--color-blur-fallback);
+
+    .popup {
+      $max-width: 768px;
+
+      max-width: $max-width;
+      padding: 1em;
+
+      &.fill {
+        width: $max-width;
+      }
+    }
+  }
+
+  /* if backdrop support: very transparent and blurred */
+  @supports ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+    .overlay {
+      background-color: transparent;
+      backdrop-filter: var(--filter-bg-blur);
+      -webkit-backdrop-filter: var(--filter-bg-blur);
+    }
+  }
+</style>
