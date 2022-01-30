0baf7d7cc327471daaeb5a73da03a847c7fc4d2e
Frank Kulak <fs.kulak@gmail.com>
2022-01-30 00:56:04 -0500

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,14 +8,89 @@ interface PackageData {
   indexLink: string;
 }
 
-type DocsIndexData = {
-  title: string;
-  entries: string[];
-}[];
-
 interface DocsPageParams {
   package: string;
   version: string;
   section: string;
   entry: string;
+}
+
+interface DocsIndexData {
+  versions: string[];
+  groups: {
+    group: string;
+    items: string[];
+  }[];
+};
+
+interface DocsContentData {
+  imports?: string[];
+  header: DocsHeader;
+  sections: DocsSection[];
+  seeAlso?: DocsTypeReference[];
+}
+
+interface DocsHeader {
+  description: string;
+  repoUrl?: string;
+  title?: {
+    prefix?: string;
+    name: string;
+    extends?: DocsTypeReference[];
+    implements?: DocsTypeReference[];
+    generics?: DocsTypeGeneric[];
+  }
+}
+
+interface DocsSection {
+  title?: string;
+  description?: string;
+  content: DocsSectionContent[];
+}
+
+interface DocsSectionContent {
+  displayType: "property" | "function" | "enum";
+}
+
+interface DocsSectionProperty extends DocsSectionContent {
+  displayType: "property";
+  prefix?: string;
+  name: string;
+  type: DocsTypeReference;
+  description?: string;
+  optional?: boolean;
+}
+
+interface DocsSectionFunction extends DocsSectionContent {
+  displayType: "function";
+  prefix?: string;
+  name: string;
+  generics?: DocsTypeGeneric[];
+  returnType?: DocsTypeReference;
+  description?: string;
+  arguments?: {
+    name: string;
+    type: DocsTypeReference;
+    description?: string;
+    optional?: boolean;
+  }[];
+  exampleCode?: string;
+}
+
+interface DocsSectionEnum extends DocsSectionContent {
+  displayType: "enum";
+  values: {
+    key: string;
+    value: string;
+  }[];
+}
+
+interface DocsTypeReference {
+  name: string;
+  path?: string;
+}
+
+interface DocsTypeGeneric {
+  name: string;
+  extends?: DocsTypeReference[];
 }
