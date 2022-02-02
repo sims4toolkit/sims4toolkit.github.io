4167cdde52f50b92141b0bc3410875200aa4519c
Frank Kulak <fs.kulak@gmail.com>
2022-02-01 20:25:02 -0500

    Pin models package to top of docs.

--- src/components/pages/docs/DocsLandingPage.svelte
+++ src/components/pages/docs/DocsLandingPage.svelte
@@ -9,8 +9,21 @@
   <ContentArea>
     <div class="docs-links-wrapper">
       {#each Object.entries(documentation) as [path, props], key (key)}
-        <a href="/docs/{path}" use:link class="move-on-hover">
+        <a
+          href="/docs/{path}"
+          use:link
+          class="move-on-hover"
+          class:pinned={props.pinned}
+        >
           <div class="docs-link-container floating-card drop-shadow">
+            {#if props.pinned}
+              <img
+                class="pin"
+                src="../assets/star.svg"
+                alt="pin"
+                title="This is a popular package!"
+              />
+            {/if}
             <span class="pkg-name"><SectionHeader title="@s4tk/{path}" /></span>
             <p>{props.description}</p>
           </div>
@@ -37,11 +50,25 @@
     a {
       text-decoration: none;
       color: var(--color-text);
-      flex: 40%;
+      flex: 45%;
+
+      &.pinned {
+        order: -1;
+        flex: 100%;
+      }
     }
 
     .docs-link-container {
       height: 100%;
+
+      img.pin {
+        position: absolute;
+        top: 1em;
+        right: 1em;
+        width: 24px;
+        filter: invert(87%) sepia(47%) saturate(951%) hue-rotate(331deg)
+          brightness(98%) contrast(87%);
+      }
     }
   }
 </style>
