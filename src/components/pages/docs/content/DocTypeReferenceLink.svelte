28d54c5821d1f9aa5e7a52da99e0aa57b7fff55e
Frank Kulak <fs.kulak@gmail.com>
2022-04-26 22:09:52 -0400

    Update DocTypeReferenceLink.svelte

--- src/components/pages/docs/content/DocTypeReferenceLink.svelte
+++ src/components/pages/docs/content/DocTypeReferenceLink.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
   import { getContext } from "svelte";
-  import { link } from "svelte-spa-router";
 
   export let typeRef: DocsTypeReference;
 
   const context: any = getContext("docs");
 </script>
 
-{#if typeRef.path}
-  <a href={context.getHrefForPath(typeRef.path)}>{typeRef.name}</a>
-{:else}
-  {typeRef.name}
-{/if}
+<span>
+  {#if typeRef.path}
+    <a href={context.getHrefForPath(typeRef.path)}>{typeRef.name}</a>
+  {:else}
+    {typeRef.name}
+  {/if}
+</span>
 
 <style lang="scss">
-  // intentionally blank
+  span:not(:last-child):after {
+    content: ", ";
+  }
 </style>
